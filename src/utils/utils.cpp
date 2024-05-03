b8e088056a1bd48e6fdfd5e71d07b9d4abdd7d20
tlouro-c <tomascorreia.lc@gmail.com>
2024-05-03 11:58:47 +0000

    Added: Frontend Improvements

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -82,9 +82,7 @@ std::string	basicHtml(const std::string& title, const std::string& body) {
 		"<html>\n"
 		"<head>\n"
 			"\t<title>" + title + "</title>\n"
-			"\t<style>\n"
-				"\t\tbody {font-family: Arial, Helvetica, sans-serif; font-size: 16px;}\n"
-			"\t</style>\n"
+			"\t<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">"
 		"</head>\n"
 		"<body>\n"
 			+ body +
